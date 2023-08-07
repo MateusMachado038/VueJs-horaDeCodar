2d9d6893347eddb72713584ec0d6558d744538ac
root <root@pop-os.localdomain>
2023-08-06 23:59:40 -0300

    Usando Emit

--- 2-vue-cli/src/components/Picture.vue
+++ 2-vue-cli/src/components/Picture.vue
@@ -1,15 +1,26 @@
 <template>
   <img :src="avatar" :alt="descricao" />
+  <MudarImagem @mudar-imagem="mudarImagem" />
 </template>
 
 <script>
+import MudarImagem from "./MudarImagem.vue";
+
 export default {
   name: "Picture_01",
+  components: {
+    MudarImagem,
+  },
   data() {
     return {
       avatar: "/img/avatar.png",
       descricao: "Mateus Alves Machado",
     };
+  },
+  methods: {
+    mudarImagem() {
+      this.avatar = "/img/avatar2.png";
+    },
   },
 };
 </script>
