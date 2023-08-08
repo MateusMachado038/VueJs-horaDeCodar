09e132027a62d583ce051530f386bf58cf4aee6b
root <root@pop-os.localdomain>
2023-08-08 18:40:31 -0300

    criando primeiros componentes

--- make_your_burger/src/App.vue
+++ make_your_burger/src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <NavBarVue />
+    <router-view />
+    <FooterVue />
+  </div>
 </template>
 
+<script>
+import NavBarVue from "./components/NavBar.vue";
+import FooterVue from "./components/Footer.vue";
+
+export default {
+  components: {
+    NavBarVue,
+    FooterVue,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
