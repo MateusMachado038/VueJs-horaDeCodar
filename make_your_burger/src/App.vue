b46f64f9d0eed2123cc21b1b61b2e8609b1e102a
root <root@pop-os.localdomain>
2023-08-09 00:13:31 -0300

    desenvolvendo as paginas home e pedidos

--- make_your_burger/src/App.vue
+++ make_your_burger/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NavBarVue />
+    <NavBarVue :logo="logo_src" :alt="app_name" />
     <router-view />
     <FooterVue />
   </div>
@@ -15,8 +15,32 @@ export default {
     NavBarVue,
     FooterVue,
   },
+  data() {
+    return {
+      logo_src: ".//assets/img/logo.png",
+      app_name: "Mae Your Burger",
+    };
+  },
 };
 </script>
 
 <style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
